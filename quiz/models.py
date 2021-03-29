bbf8f5136e5a67f23ef819081735a78ec31f2e55
jong <jong0342@gmail.com>
2021-03-29 16:27:54 +0900

    Ready for deploy

---
+++ quiz/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    answer = models.IntegerField()
